dc0277a38862ee57406c7e9d05f07585a9736d81
PyroKsen <maxmax11219@yandex.ru>
2023-10-31 13:59:39 +0300

    ura2

--- numbers.js
+++ numbers.js
@@ -7,21 +7,21 @@ export const calculateKilobytes = (a) => Math.floor(a / 1024);
 export const calculateSegments = (a, b) => Math.floor(a / b);
 
 export const calculateDigitSum = (str) => {
-    let d = String(str)
-    let r1 = Number(d[0])
-    let r2 = Number(d[1])
-    let b = r1 + r2
-    return b
+  const d = String(str);
+  const r1 = Number(d[0]);
+  const r2 = Number(d[1]);
+  const b = r1 + r2;
+  return b;
 };
 
 export const swapHundredsAndTens = (str) => {
-    let d = String(str)
-    let r1 = String(d[0])
-    let r2 = String(d[1])
-    let r3 = String(d[2])
-    let b = r2 + r1 + r3
-    let s = Number(b)
-    return s
+  const d = String(str);
+  const r1 = String(d[0]);
+  const r2 = String(d[1]);
+  const r3 = String(d[2]);
+  const b = r2 + r1 + r3;
+  const s = Number(b);
+  return s;
 };
 
 export const getHundredsDigit = () => null;
