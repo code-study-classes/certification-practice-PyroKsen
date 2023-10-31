cc850980c5c6c46b523135d833f14d6f6c1fec7b
PyroKsen <maxmax11219@yandex.ru>
2023-10-31 13:58:52 +0300

    ura

--- numbers.js
+++ numbers.js
@@ -1,1 +1,33 @@
+export const calculateDistance = (a, b) => Math.abs(a - b);
 
+export const calculateSegmentProduct = (a, b, c) => Math.abs(a - c) * Math.abs(c - b);
+
+export const calculateKilobytes = (a) => Math.floor(a / 1024);
+
+export const calculateSegments = (a, b) => Math.floor(a / b);
+
+export const calculateDigitSum = (str) => {
+    let d = String(str)
+    let r1 = Number(d[0])
+    let r2 = Number(d[1])
+    let b = r1 + r2
+    return b
+};
+
+export const swapHundredsAndTens = (str) => {
+    let d = String(str)
+    let r1 = String(d[0])
+    let r2 = String(d[1])
+    let r3 = String(d[2])
+    let b = r2 + r1 + r3
+    let s = Number(b)
+    return s
+};
+
+export const getHundredsDigit = () => null;
+
+export const getFullHours = () => null;
+
+export const getDayOfWeek = () => null;
+
+export const countSquares = () => null;
