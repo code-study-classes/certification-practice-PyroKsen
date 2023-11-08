9df599ad5f656f54d42146316179ad3524e4959e
PyroKsen <maxmax11219@yandex.ru>
2023-11-08 17:30:48 +0300

    s2s

--- boolean.js
+++ boolean.js
@@ -1,71 +1,19 @@
-export const isPositive = (a) => {
-    if (a > 0) {
-        return true
-    } else {
-        return false
-    }
-};
+export const isPositive = (a) => Boolean(a > 0);
 
-export const isOdd = (a) => {
-    if (a % 2 === 1 || a % 2 === -1) {
-        return true
-    } else {
-        return false
-    }
-};
+export const isOdd = (a) => Boolean(a % 2 === 1 || a % 2 === -1);
 
-export const checkInequality = (a, b) => {
-    if (a > 2 && b <= 3) {
-        return true
-    } else {
-        return false
-    }
-};
+export const checkInequality = (a, b) => Boolean(a > 2 && b <= 3);
 
-export const checkInequality2 = (a, b) => {
-    if (a >= 0 || b < -2) {
-        return true
-    } else {
-        return false
-    }
-};
+export const checkInequality2 = (a, b) => Boolean(a >= 0 || b < -2);
 
-export const checkBetweenNumbers = (a, b, c) => {
-    if (b > a && b < c) {
-        return true
-    } else {
-        return false
-    }
-};
+export const checkBetweenNumbers = (a, b, c) => Boolean(b > a && b < c);
 
-export const checkOddThreeDigitNumber = (a) => {
-    if ((a < -99 && a > -1000 || a < 1000 && a > 99)) {
-        if (a % 2 === 1) {
-            return true
-        }
-        return false
-    }
-    return false
-};
+export const checkOddThreeDigitNumber = (a) => Boolean((a < -99 && a > -1000 && a % 2 === 1) || (a < 1000 && a > 99 && a % 2 === 1));
 
 export const checkUniqueDigits = () => {};
 
-export const isSecondQuadrant = (x, y) => {
-    if (x < 0 && y > 0) {
-        return true
-    } else {
-        return false
-    }
-};
+export const isSecondQuadrant = (x, y) => Boolean(x < 0 && y > 0);
 
-export const isIsoscelesTriangle = (a, b, c) => {
-    if (a === 0 || b === 0 || c === 0) {
-        return false
-    } else if (a === b || b === c || a === c) {
-        return true
-    } else {
-        return false
-    }
-};
+export const isIsoscelesTriangle = (a, b, c) => Boolean((a === b || b === c || a === c) && a + b + c !== 0);
 
 export const areSameColorFields = () => {};
